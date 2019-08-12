15d2072d44a19ccd065f93a4550385cda7b9336f
Andrea Spadaccini <andrea.spadaccini@gmail.com>
2019-08-12 16:19:06 +0100

    Getting data directly from OnHub

--- parse.py
+++ parse.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+import requests
 
 DEBUG = 0
 
@@ -6,14 +7,13 @@ def debug(s):
     if DEBUG != 0:
         print(s)
 
-def get_stations(filename):
-    f = open(filename, 'rb')
+def get_stations(binary_response_from_onhub):
     # map DHCP hostname to list of station_info
     stations = defaultdict(list)
     capturing = False
     parens_count = 0
     tmp = {}
-    for i, raw_line in enumerate(f.readlines()):
+    for i, raw_line in enumerate(binary_response_from_onhub.split(b"\n")):
         # We are opening a binary file, some lines won't be text and
         # it's ok to ignore them, as the data we care about is in the
         # text portion of the file.
@@ -24,7 +24,7 @@ def get_stations(filename):
 
         if capturing:
             data = line.strip()
-            debug(data)
+            debug("Line %i: -%s-" % ((i+1), data))
             if data.endswith("{"):
                 parens_count += 1
                 debug("incremented parens_count to %d" % parens_count)
@@ -45,6 +45,8 @@ def get_stations(filename):
                 if value:
                     debug("Adding %s, %s" % (key, value))
                     tmp[key] = value
+                else:
+                    debug("Empty key/value pair")
                 continue
 
         if "station_info" in line:
@@ -56,10 +58,31 @@ def get_stations(filename):
 
     return stations
 
+def get_onhub_data():
+    # Mimic a browser. Data obtained by converting a request from the browser to
+    # cURL format and then converted to requests syntax with https://curl.trillworks.com/
+    headers = {
+        'Connection': 'keep-alive',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'en-US,en;q=0.9,it;q=0.8',
+    }
 
-stations = get_stations("diag")
+    debug("Connecting to OnHub")
+    response = requests.get('http://onhub.here/api/v1/diagnostic-report',
+                            headers=headers,
+                            verify=False)
+    return response.content
 
-connected_stations = [name for name, data in stations.items()
-        if all(d["connected"] == "true" for d in data)]
+def get_connected_stations():
+    data = get_onhub_data()
+    debug("Parsing request")
+    stations = get_stations(data)
 
-print(connected_stations)
+    connected_stations = [name for name, data in stations.items()
+            if all(d["connected"] == "true" for d in data)]
+    return connected_stations
+
+print(get_connected_stations())
